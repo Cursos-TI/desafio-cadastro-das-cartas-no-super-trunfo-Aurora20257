ef0469c771b4d54866a9de82aba01b92d8521da4
Aurora20257 <estudioavanco2025@gmail.com>
2025-02-15 17:43:29 +0000

    Informações adicionais

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+
+/* 🎮 Nível Novato
+No nível Novato, você iniciará criando o sistema básico do jogo Super Trunfo com o tema "Países". As cartas serão divididas por estados, cada um com quatro cidades.
+
+Cada país será dividido em oito estados, identificados pelas letras de A a H. Cada estado terá quatro cidades, numeradas de 1 a 4. A combinação da letra do estado 
+e o número da cidade define o código da carta (por exemplo, A01, A02, B01, B02).
+
+🚩 Objetivo:
+Criar cartas representando cidades, contendo as seguintes propriedades:
+População
+Área
+PIB
+Número de pontos turísticos
+
+⚙️ Funcionalidades do Sistema:
+O sistema permitirá ao usuário cadastrar cartas de cidades, inserindo manualmente os dados via terminal de comando.
+Após o cadastro, o sistema exibirá os dados de cada cidade de forma clara e organizada.
+
+📥 Entrada e 📤 Saída de Dados:
+O usuário insere os dados de cada carta interativamente.
+Após o cadastro, os dados são exibidos com todas as propriedades da cidade, uma por linha.*/
+
+
 int main(){
 
 //Definições das informações do estado
