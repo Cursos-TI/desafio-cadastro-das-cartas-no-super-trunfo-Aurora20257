b010f29cc3fa76017f23f9ca0bb0b53da8e41f3b
Aurora20257 <estudioavanco2025@gmail.com>
2025-02-17 20:45:58 +0000

    Título

---
+++ TESTMODI_long.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+int numeroNormal = 2100000047; // Valor máximo de int
+long int numeroGrande = 21474836647;
+
+printf("Número regular (int): %d\n", numeroNormal);
+printf("Número grande (long int): %ld\n", numeroGrande);
+
+return 0;
+
+}
